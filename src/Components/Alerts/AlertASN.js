ee3599114f452dee47cdc0b16d725742e3c6993d
Syed Ali <sufiyaanjam7@gmail.com>
2023-11-09 15:19:35 +0530

    hg

---
+++ src/Components/Alerts/AlertASN.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import HomeNav from '../Navbar/HomeNav'
+import AlertNavbar from '../Navbar/AlertNavbar'
+
+function AlertASN() 
+{
+    return (
+      <div>
+        {<HomeNav/>}
+        {<AlertNavbar/>}
+
+      </div>
+    )
+}
+
+export default AlertASN

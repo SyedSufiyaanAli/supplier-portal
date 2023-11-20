d68408afd5f1760c95eeab465d87ea856b5d6c72
Syed Ali <sufiyaanjam7@gmail.com>
2023-11-20 15:37:47 +0530

    davad

--- src/Components/Buttons/UploadFile.js
+++ src/Components/Buttons/UploadFile.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import '../../style/UploadFile.css'
-import { FaFileUpload } from "react-icons/fa";
 
 function UploadButton() {
   return (
     <div class="upload-btn-wrapper">
-      <button class="btn">Upload a file<i style="font-size:18px" class="fa">&#xf093;</i></button>
+      <button class="Uploadbtn">Upload a file <i class="fa"> &#xf093; </i></button>
       <input type="file" name="myfile" />
   </div>
   )
